fc16eb5dab84b86933f70597661487dc733469c1
Werner Johansson <wj@unifiedengineering.se>
2015-06-18 23:43:31 -0700

    Also extract metadata from supplied firmware file

--- cymetadata.c
+++ cymetadata.c
@@ -1,24 +1,60 @@
 // cybootloaderutils lacks an implementation of the GetMetadata command
-// Here's one, structured like the other cybootloaderutils commands 
+// Here's one, mostly structured like the other cybootloaderutils commands 
 // Werner Johansson <wj@unifiedengineering.se>
 
 #include <stdint.h>
 #include "cymetadata.h"
 #include <cybtldr_command.h>
 #include <cybtldr_api.h>
+#include <cybtldr_parse.h>
 
-int CyBtldr_GetMetadata(uint8_t bootloadableNum, uint32_t* appCustId, uint16_t* appId, uint16_t* appVer, uint16_t* bootVer) {
+int CyBtldr_GetMetadata(CyBtldr_CommunicationsData* cyComms, const char* filename, uint8_t bootloadableNum, cymeta_t* meta) {
     uint16_t inSize = 0;
     uint16_t outSize = 0;
     uint8_t inBuf[MAX_COMMAND_SIZE];
     uint8_t outBuf[MAX_COMMAND_SIZE];
     int err;
 
-    err = CyBtldr_CreateGetMetadataCmd(inBuf, &inSize, &outSize, bootloadableNum);
+    err = CyBtldr_OpenDataFile(filename);
+    if (CYRET_SUCCESS == err) {
+        unsigned long blVer = 0;
+        unsigned long siliconId = 0;
+        unsigned char siliconRev = 0;
+        CyBtldr_ChecksumType chksumtype = SUM_CHECKSUM;
+        char line[MAX_BUFFER_SIZE];
+        unsigned int lineLen;
+        uint8_t* securityKey = NULL;
+        meta->appId = meta->appVer = meta->bootVer = meta->fileAppId = meta->fileAppVer = 0;
+        meta->appCustId = meta->fileAppCustId = 0;
+
+        err = CyBtldr_ReadLine(&lineLen, line);
+        if (CYRET_SUCCESS == err)
+            err = CyBtldr_ParseHeader(lineLen, (uint8_t*)line, &siliconId, &siliconRev, (uint8_t*)&chksumtype);
+
+        if (CYRET_SUCCESS == err) {
+            CyBtldr_SetCheckSumType(chksumtype);
+            while( CYRET_SUCCESS == (err = CyBtldr_ReadLine(&lineLen, line)) ) {
+                uint8_t dummyArrayId, dummyChecksum;
+                uint16_t dummyRowNum, buffersize = sizeof(line);
+                err = CyBtldr_ParseRowData(lineLen, (uint8_t*)line, &dummyArrayId, &dummyRowNum, (uint8_t*)line, &buffersize, &dummyChecksum);
+                if(CYRET_SUCCESS == err) {
+                    meta->fileAppCustId = (line[195 + BTLDB_APP_CUST_ID_OFFSET] << 24) | (line[194 + BTLDB_APP_CUST_ID_OFFSET] << 16) |
+                                    (line[193 + BTLDB_APP_CUST_ID_OFFSET] << 8) | line[192 + BTLDB_APP_CUST_ID_OFFSET];
+                    meta->fileAppId = (line[193 + BTLDB_APP_ID_OFFSET] << 8) | line[192 + BTLDB_APP_ID_OFFSET];
+                    meta->fileAppVer = (line[193 + BTLDB_APP_VERSION_OFFSET] << 8) | line[192 + BTLDB_APP_VERSION_OFFSET];
+                }
+            }
+            err = CyBtldr_StartBootloadOperation(cyComms, siliconId, siliconRev, &blVer, securityKey);
+        }
+    }
+    CyBtldr_CloseDataFile();
+
+    if (CYRET_SUCCESS == err)
+        err = CyBtldr_CreateGetMetadataCmd(inBuf, &inSize, &outSize, bootloadableNum);
     if (CYRET_SUCCESS == err)
         err = CyBtldr_TransferData(inBuf, inSize, outBuf, outSize);
     if (CYRET_SUCCESS == err)
-        err = CyBtldr_ParseGetMetadataCmdResult(outBuf, outSize, appCustId, appId, appVer, bootVer);
+        err = CyBtldr_ParseGetMetadataCmdResult(outBuf, outSize, meta);
 
     return err;
 }
@@ -43,7 +79,7 @@ int CyBtldr_CreateGetMetadataCmd(uint8_t* cmdBuf, uint16_t* cmdSize, uint16_t* r
     return CYRET_SUCCESS;
 }
 
-int CyBtldr_ParseGetMetadataCmdResult(uint8_t* cmdBuf, uint16_t cmdSize, uint32_t* appCustId, uint16_t* appId, uint16_t* appVer, uint16_t* bootVer) {
+int CyBtldr_ParseGetMetadataCmdResult(uint8_t* cmdBuf, uint16_t cmdSize, cymeta_t* meta) {
     const uint16_t RESULT_DATA_SIZE = 56;
     const uint16_t RESULT_SIZE = BASE_CMD_SIZE + RESULT_DATA_SIZE;
     int err = CYRET_SUCCESS;
@@ -55,15 +91,11 @@ int CyBtldr_ParseGetMetadataCmdResult(uint8_t* cmdBuf, uint16_t cmdSize, uint32_
     else if (cmdBuf[0] != CMD_START || cmdBuf[2] != RESULT_DATA_SIZE || cmdBuf[3] != (RESULT_DATA_SIZE >> 8) || cmdBuf[RESULT_SIZE - 1] != CMD_STOP)
         err = CYRET_ERR_DATA;
     else {
-        if(appCustId)
-            *appCustId = (cmdBuf[7 + BTLDB_APP_CUST_ID_OFFSET] << 24) | (cmdBuf[6 + BTLDB_APP_CUST_ID_OFFSET] << 16) |
+        meta->appCustId = (cmdBuf[7 + BTLDB_APP_CUST_ID_OFFSET] << 24) | (cmdBuf[6 + BTLDB_APP_CUST_ID_OFFSET] << 16) |
                 (cmdBuf[5 + BTLDB_APP_CUST_ID_OFFSET] << 8) | cmdBuf[4 + BTLDB_APP_CUST_ID_OFFSET];
-        if(appId)
-            *appId = (cmdBuf[5 + BTLDB_APP_ID_OFFSET] << 8) | cmdBuf[4 + BTLDB_APP_ID_OFFSET];
-        if(appVer)
-            *appVer = (cmdBuf[5 + BTLDB_APP_VERSION_OFFSET] << 8) | cmdBuf[4 + BTLDB_APP_VERSION_OFFSET];
-        if(bootVer)
-            *bootVer = (cmdBuf[5 + BTLDR_APP_VERSION_OFFSET] << 8) | cmdBuf[4 + BTLDR_APP_VERSION_OFFSET];
+        meta->appId = (cmdBuf[5 + BTLDB_APP_ID_OFFSET] << 8) | cmdBuf[4 + BTLDB_APP_ID_OFFSET];
+        meta->appVer = (cmdBuf[5 + BTLDB_APP_VERSION_OFFSET] << 8) | cmdBuf[4 + BTLDB_APP_VERSION_OFFSET];
+        meta->bootVer = (cmdBuf[5 + BTLDR_APP_VERSION_OFFSET] << 8) | cmdBuf[4 + BTLDR_APP_VERSION_OFFSET];
     }
 
     return err;
--- cymetadata.h
+++ cymetadata.h
@@ -2,6 +2,17 @@
 #define CYADDON_H
 
 #include <stdint.h>
+#include <cybtldr_api.h>
+
+typedef struct {
+    uint16_t appId;
+    uint16_t appVer;
+    uint16_t bootVer;
+    uint16_t fileAppId;
+    uint16_t fileAppVer;
+    uint32_t appCustId;
+    uint32_t fileAppCustId;
+} cymeta_t;
 
 // Get Metadata command not included in current version of cybootloaderutils
 #define CMD_GET_METADATA 0x3C
@@ -14,9 +25,9 @@
 #define BTLDB_APP_VERSION_OFFSET (22) // 2 bytes
 #define BTLDB_APP_CUST_ID_OFFSET (24) // 4 bytes
 
-int CyBtldr_GetMetadata(uint8_t bootloadableNum, uint32_t* appCustId, uint16_t* appId, uint16_t* appVer, uint16_t* bootVer);
+int CyBtldr_GetMetadata(CyBtldr_CommunicationsData* cyComms, const char* filename, uint8_t bootloadableNum, cymeta_t* meta);
 int CyBtldr_CreateGetMetadataCmd(uint8_t* cmdBuf, uint16_t* cmdSize, uint16_t* resSize, uint8_t bootloadableNum);
-int CyBtldr_ParseGetMetadataCmdResult(uint8_t* cmdBuf, uint16_t cmdSize, uint32_t* appCustId, uint16_t* appId, uint16_t* appVer, uint16_t* bootVer);
+int CyBtldr_ParseGetMetadataCmdResult(uint8_t* cmdBuf, uint16_t cmdSize, cymeta_t* meta);
 
 #endif // CYADDON_H
 
